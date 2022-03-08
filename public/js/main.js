0c19d428e1a6608e2f95250f6fb804df0b8c6fa2
Enrique Castañeda <mrstinky64@gmail.com>
2022-03-08 12:08:55 -0700

    las tareas ya pueden ser eliminadas

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function(){
 
+
     async function getTareas(){
 
         const result = await $.ajax({
@@ -17,15 +18,31 @@ $(document).ready(function(){
         const result = await $.ajax({
             type: "POST",
             url: "http://localhost:3000/tareas",
-            data: tarea,
+            data: JSON.stringify(tarea),
+            dataType: "json",
+            contentType: 'application/json',
             async: true,
             
         });
 
+        render(result);
         return result;
     }
 
-    $('#nuevaTareaBtn').click(async function(e){
+    function borrarTarea(id){
+        $.ajax({
+            type: "PATCH",
+            url: "http://localhost:3000/tareas/"+id,
+            data: JSON.stringify({"visible":false}),
+            dataType: "json",
+            contentType: 'application/json',
+            success: function (response) {
+                
+            }
+        });
+    }
+
+    $('#nuevaTareaBtn').on('click',async function(e){
 
         e.preventDefault();
 
@@ -35,45 +52,63 @@ $(document).ready(function(){
             "fechaFin":$('#tareaFechaFin').val(),
             "duracion":$('#tareaDuracion').val(),
             "tiempoRegistrado": "00:00",
-            "status": "pendiente"
+            "status": "pendiente",
+            "visible": true
         }
 
-        const res = await newTarea(tarea);
-        render(res);
+        newTarea(tarea);
+        
+
+        $("#nuevaTareaForm")[0].reset();
         //if(key){buscarPorIndex(key);}
 
     });
+
+    
 
     async function renderisarTareas(){
         const res = await getTareas();
         res.forEach(element => {
             render(element);
         });
+
+        
     }
 
     function render(element){
-        var card = "<div class='col'>\n\
-                            <div class='card position-relative shadow'>\n\
-                                <div class='card-body'>\n\
-                                    <button type='button' class='btn-close position-absolute top-0 end-0'style='margin:1em;' aria-label='Close'></button>\n\
-                                    <h5 class='card-title'>"+element.nombre+"</h5>\n\
-                                    <p class='card-text'>"+element.descripcion+"</p>\n\
+        if(element.visible){
+            var card = "<div class='col' id=card"+element.id+">\n\
+                                <div class='card position-relative shadow'>\n\
+                                    <div class='card-body'>\n\
+                                        <button type='button' class='borrarTarea btn-close position-absolute top-0 end-0' style='margin:1em;' aria-label='Close' tareaId="+element.id+" id=btnBorrar"+element.id+"></button>\n\
+                                        <h5 class='card-title'>"+element.nombre+"</h5>\n\
+                                        <p class='card-text'>"+element.descripcion+"</p>\n\
+                                    </div>\n\
+                                    <ul class='list-group list-group-flush'>\n\
+                                        <li class='list-group-item'>Estado: "+element.status+"</li>\n\
+                                        <li class='list-group-item'>Fecha de fin: "+element.fechaFin+"</li>\n\
+                                        <li class='list-group-item'>Duracion: "+element.duracion+" min</li>\n\
+                                        <li class='list-group-item'>Tiempo registrado: "+element.tiempoRegistrado+"</li>\n\
+                                    </ul>\n\
+                                    <div class='card-body mx-auto'>\n\
+                                        <button type='button' class='btn btn-outline-primary'>Iniciar</button>\n\
+                                        <button type='button' class='btn btn-outline-warning''>Pausar</button>\n\
+                                        <button type='button' class='btn btn-outline-danger''>Detener</button>\n\
+                                    </div>\n\
                                 </div>\n\
-                                <ul class='list-group list-group-flush'>\n\
-                                    <li class='list-group-item'>Estado: "+element.status+"</li>\n\
-                                    <li class='list-group-item'>Fecha de fin: "+element.fechaFin+"</li>\n\
-                                    <li class='list-group-item'>Duracion: "+element.duracion+" min</li>\n\
-                                    <li class='list-group-item'>Tiempo registrado: "+element.tiempoRegistrado+"</li>\n\
-                                </ul>\n\
-                                <div class='card-body mx-auto'>\n\
-                                    <button type='button' class='btn btn-outline-primary'>Iniciar</button>\n\
-                                    <button type='button' class='btn btn-outline-warning''>Pausar</button>\n\
-                                    <button type='button' class='btn btn-outline-danger''>Detener</button>\n\
-                                </div>\n\
-                            </div>\n\
-                        </div>";
-                        
-        $('#tareasCardsDiv').append(card);
+                            </div>";
+
+            $('#tareasCardsDiv').append(card);
+
+            $('#btnBorrar'+element.id).on('click',function(e){
+                e.preventDefault();
+
+                var id= '#card'+element.id;
+                $(id).remove();
+                borrarTarea(element.id);
+
+            });
+        }
     }
 
     renderisarTareas();
